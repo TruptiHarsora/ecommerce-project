5fa84a2c06381997571c6532113c866bf29b2463
TruptiHarsora <231615134+TruptiHarsora@users.noreply.github.com>
2026-09-07 12:24:26 +0530

    Refactor image compression settings for better quality

--- server/utils/compressImage.js
+++ server/utils/compressImage.js
@@ -1,10 +1,20 @@
 const sharp = require("sharp");
 
 const compressImage = async (buffer) => {
-    return await sharp(buffer)
-        .resize(12000)
-        .jpeg({ quality: 70 })
-        .toBuffer();
+  return await sharp(buffer)
+    // .resize(12000)
+    // .jpeg({ quality: 70 })
+    .resize({
+      width: 1200,
+      height: 1200,
+      fit: "inside",
+      withoutEnlargement: true,
+    })
+    .jpeg({
+      quality: 70,
+      progressive: true,
+    })
+    .toBuffer();
 };
 
 module.exports = compressImage;
